4d29a3a739d90051cacd2ae4e43b309955d08be8
Alberto Pluecker <albertopluecker@gmail.com>
2023-04-08 23:28:44 +0200

    Added Navbar

--- MFUIR_AT-2/src/App.jsx
+++ MFUIR_AT-2/src/App.jsx
@@ -1,9 +1,36 @@
 import './App.css'
+import Grid2 from '@mui/material/Unstable_Grid2/Grid2'
+import Navbar from './components/Navbar'
 
 function App() {
 	return (
 		<div className='App'>
-			<h1>Test</h1>
+			<Grid2 container>
+				<Grid2
+					xs={12}
+					md={12}
+				>
+					<Navbar />
+				</Grid2>
+				<Grid2
+					xs={0}
+					md={2}
+				>
+					Sidebar
+				</Grid2>
+				<Grid2
+					xs={12}
+					md={8}
+				>
+					Feed
+				</Grid2>
+				<Grid2
+					xs={0}
+					md={2}
+				>
+					Friendsbar
+				</Grid2>
+			</Grid2>
 		</div>
 	)
 }
---
+++ MFUIR_AT-2/src/components/Navbar.jsx
@@ -0,0 +1,103 @@
+import { Mail, Notifications } from '@mui/icons-material'
+import BlurCircularRoundedIcon from '@mui/icons-material/BlurCircularRounded'
+import {
+	AppBar,
+	Avatar,
+	Badge,
+	Box,
+	InputBase,
+	Menu,
+	MenuItem,
+	styled,
+	Toolbar,
+	Typography
+} from '@mui/material'
+import React, { useState } from 'react'
+
+const StyledToolbar = styled(Toolbar)({
+	display: 'flex',
+	justifyContent: 'space-between'
+})
+
+const Search = styled('div')(({ theme }) => ({
+	backgroundColor: 'white',
+	padding: '0 10px',
+	borderRadius: theme.shape.borderRadius,
+	width: '35%'
+}))
+
+const Icons = styled(Box)(({ theme }) => ({
+	display: 'none',
+	alignItems: 'center',
+	gap: '20px',
+	[theme.breakpoints.up('sm')]: {
+		display: 'flex'
+	}
+}))
+
+const UserBox = styled(Box)(({ theme }) => ({
+	display: 'flex',
+	alignItems: 'center',
+	gap: '10px',
+	[theme.breakpoints.up('sm')]: {
+		display: 'none'
+	}
+}))
+const Navbar = () => {
+	const [open, setOpen] = useState(false)
+	return (
+		<AppBar position='sticky'>
+			<StyledToolbar>
+				<Typography
+					variant='h6'
+					sx={{ display: { xs: 'none', sm: 'block' } }}
+				>
+					REDE SOCIAL
+				</Typography>
+				<BlurCircularRoundedIcon
+					sx={{ display: { xs: 'block', sm: 'none', cursor: 'pointer' } }}
+				/>
+				<Search>
+					<InputBase placeholder='pesquisar...' />
+				</Search>
+				<Icons>
+					<Badge
+						badgeContent={9}
+						color='error'
+					>
+						<Notifications />
+					</Badge>
+					<Badge
+						badgeContent={6}
+						color='error'
+					>
+						<Mail />
+					</Badge>
+
+					<Avatar
+						sx={{ width: 30, height: 30, cursor: 'pointer' }}
+						onClick={(e) => setOpen(true)}
+					/>
+				</Icons>
+				<UserBox onClick={(e) => setOpen(true)}>
+					<Avatar sx={{ width: 30, height: 30, cursor: 'pointer' }} />
+					<Typography variant='span'>Alberto</Typography>
+				</UserBox>
+			</StyledToolbar>
+			<Menu
+				open={open}
+				onClose={(e) => setOpen(false)}
+				anchorOrigin={{
+					vertical: 'center',
+					horizontal: 'right'
+				}}
+			>
+				<MenuItem>Perfil</MenuItem>
+				<MenuItem>Minha conta</MenuItem>
+				<MenuItem>Sair</MenuItem>
+			</Menu>
+		</AppBar>
+	)
+}
+
+export default Navbar
